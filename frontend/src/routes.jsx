9fc3e803b1aab6a759e58855c30cb902a6ddeba7
Firdose Shaikh <firdose0402@gmail.com>
2026-07-02 19:32:10 +0530

    Fix: Correct import filenames to match actual page components

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Attractions from './pages/Attractions';
+import Attraction from './pages/Attraction';
 import Nature from './pages/Nature';
 import FoodGuide from './pages/FoodGuide';
-import Hotels from './pages/Hotels';
+import Hotel from './pages/Hotel';
 import Gallery from './pages/Gallery';
-import Blogs from './pages/Blogs';
+import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/attractions" element={<Attractions />} />
+      <Route path="/attractions" element={<Attraction />} />
       <Route path="/nature" element={<Nature />} />
       <Route path="/food-guide" element={<FoodGuide />} />
-      <Route path="/hotels" element={<Hotels />} />
+      <Route path="/hotels" element={<Hotel />} />
       <Route path="/gallery" element={<Gallery />} />
-      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/blogs" element={<Blog />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
   );
